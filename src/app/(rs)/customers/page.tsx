79d8fd00152bd6b1f7579f86ab4e227b84351d89
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2025-06-28 17:38:27 -0300

    new queries

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import CustomerSearch from "./CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
 
 export const metadata = {
   title: "Customers",
 };
-const Customers = () => {
-  return <div>Customers</div>;
+const Customers = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+  const { search } = searchParams;
+
+  if (!search) {
+    return <CustomerSearch />;
+  }
+  const results = await getCustomerSearchResults(search);
+
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 };
 
 export default Customers;
